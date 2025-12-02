a4a58c14328efb1c72f94838e690a4d5943146e6
Julian correa <juliancorrearisti@gmail.com>
2025-12-01 21:27:54 -0500

    subieno el proyecto

---
+++ app/MoonShine/Resources/Service/Pages/ServiceDetailPage.php
@@ -0,0 +1,86 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\MoonShine\Resources\Service\Pages;
+
+use MoonShine\Laravel\Pages\Crud\DetailPage;
+use MoonShine\Contracts\UI\ComponentContract;
+use MoonShine\UI\Components\Table\TableBuilder;
+use MoonShine\Contracts\UI\FieldContract;
+use App\MoonShine\Resources\Service\ServiceResource;
+use MoonShine\Support\ListOf;
+use MoonShine\UI\Fields\ID;
+use MoonShine\UI\Fields\Text;
+use Moonshine\UI\Fields\Number;
+use MoonShine\UI\Fields\Switcher;
+use Throwable;
+
+
+/**
+ * @extends DetailPage<ServiceResource>
+ */
+class ServiceDetailPage extends DetailPage
+{
+    /**
+     * @return list<FieldContract>
+     */
+    protected function fields(): iterable
+    {
+        return [
+            ID::make(),
+            Text::make('Name'),
+            Text::make('Description'),
+            Number::make('Price'),
+           Switcher::make('Active', 'is_active'),
+        ];
+    }
+
+    protected function buttons(): ListOf
+    {
+        return parent::buttons();
+    }
+
+    /**
+     * @param  TableBuilder  $component
+     *
+     * @return TableBuilder
+     */
+    protected function modifyDetailComponent(ComponentContract $component): ComponentContract
+    {
+        return $component;
+    }
+
+    /**
+     * @return list<ComponentContract>
+     * @throws Throwable
+     */
+    protected function topLayer(): array
+    {
+        return [
+            ...parent::topLayer()
+        ];
+    }
+
+    /**
+     * @return list<ComponentContract>
+     * @throws Throwable
+     */
+    protected function mainLayer(): array
+    {
+        return [
+            ...parent::mainLayer()
+        ];
+    }
+
+    /**
+     * @return list<ComponentContract>
+     * @throws Throwable
+     */
+    protected function bottomLayer(): array
+    {
+        return [
+            ...parent::bottomLayer()
+        ];
+    }
+}
